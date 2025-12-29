2a5f0f54f3d4834104d56f9188ab985a334c8098
최주현 <iprite359@gmail.com>
2025-12-29 16:18:52 +0900

    5.3 수정

--- Source/TeamSpace/TeamSpace.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TeamSpace.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeamSpace, "TeamSpace" );
-
-DEFINE_LOG_CATEGORY(LogTeamSpace)
--- Source/TeamSpace/TeamSpaceGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TeamSpaceGameMode.h"
-
-ATeamSpaceGameMode::ATeamSpaceGameMode()
-{
-	// stub
-}
